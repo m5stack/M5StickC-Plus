e9a02d7b6ffc7c89bbe01979f2e605d378407103
Kiko62 <1905134716@qq.com>
2021-03-03 11:56:19 +0800

    Optimizing the HAT directory

---
+++ examples/Unit/Dual_Button/Dual_Button.ino
@@ -0,0 +1,72 @@
+/*
+  This Unit connects to GRVOE B on M5Core.
+*/
+#include <M5StickCPlus.h>
+
+int last_value_red = 0;
+int cur_value_red = 0;
+int last_value_blue = 0;
+int cur_value_blue = 0;
+
+void setup() {
+  // init lcd
+  M5.begin();
+  M5.Lcd.setRotation(3);
+  M5.Lcd.setTextSize(2); // title size
+  pinMode(32, INPUT);
+  pinMode(33, INPUT);
+  M5.Lcd.setTextColor(YELLOW);
+  M5.Lcd.setCursor(5, 2);
+  M5.Lcd.println("Dual Button example");
+  M5.Lcd.setTextColor(WHITE);
+
+  M5.Lcd.setTextSize(1); // text size
+}
+
+void loop() {
+  cur_value_red = digitalRead(32);
+  cur_value_blue = digitalRead(33);
+
+  M5.Lcd.setCursor(40,25); M5.Lcd.print("Blue Status: ");
+  M5.Lcd.setCursor(40,40); M5.Lcd.print("Blue Value: ");
+  M5.Lcd.setCursor(40,55); M5.Lcd.print(" Red Status: ");
+  M5.Lcd.setCursor(40,70); M5.Lcd.print(" Red Value: ");
+
+  if(cur_value_blue != last_value_blue){
+    M5.Lcd.fillRect(135,25,100,15,BLACK);
+    M5.Lcd.fillRect(135,40,100,15,BLACK);
+    if(cur_value_blue==0){
+      M5.Lcd.setCursor(135,25); M5.Lcd.print("pressed");
+      M5.Lcd.setCursor(135,40); M5.Lcd.print("0");
+      Serial.println("Button Status: blue pressed");
+      Serial.println(" value: 0");
+    }
+    else{
+      M5.Lcd.setCursor(135,25); M5.Lcd.print("released");
+      M5.Lcd.setCursor(135,40); M5.Lcd.print("1");
+      Serial.println("Button Status: blue released");
+      Serial.println(" value: 1");
+    }
+      last_value_blue = cur_value_blue;
+  }
+
+    if(cur_value_red != last_value_red){
+      M5.Lcd.fillRect(135,55,100,15,BLACK);
+      M5.Lcd.fillRect(135,70,100,15,BLACK);
+      if(cur_value_red==0){
+        M5.Lcd.setCursor(135,55); M5.Lcd.print("pressed");
+        M5.Lcd.setCursor(135,70); M5.Lcd.print("0");
+        Serial.println("Button Status: red pressed");
+        Serial.println(" value: 0");
+      }
+      else{
+        M5.Lcd.setCursor(135,55); M5.Lcd.print("released");
+        M5.Lcd.setCursor(135,70); M5.Lcd.print("1");
+        Serial.println("Button Status: red released");
+        Serial.println(" value: 1");
+      }
+      last_value_red = cur_value_red;
+  }
+
+  M5.update();
+}
---
+++ examples/Unit/HEART/MAX30100_RawData/MAX30100_RawData.ino
@@ -0,0 +1,72 @@
+/*
+    pls install MAX30100lib by library manager first
+    addr: https://github.com/oxullo/Arduino-MAX30100
+*/
+
+#include <M5StickCPlus.h>
+#include <Wire.h>
+#include "MAX30100.h"
+
+// Sampling is tightly related to the dynamic range of the ADC.
+// refer to the datasheet for further info
+#define SAMPLING_RATE                       MAX30100_SAMPRATE_100HZ
+
+// The LEDs currents must be set to a level that avoids clipping and maximises the
+// dynamic range
+#define IR_LED_CURRENT                      MAX30100_LED_CURR_50MA
+#define RED_LED_CURRENT                     MAX30100_LED_CURR_27_1MA
+
+// The pulse width of the LEDs driving determines the resolution of
+// the ADC (which is a Sigma-Delta).
+// set HIGHRES_MODE to true only when setting PULSE_WIDTH to MAX30100_SPC_PW_1600US_16BITS
+#define PULSE_WIDTH                         MAX30100_SPC_PW_1600US_16BITS
+#define HIGHRES_MODE                        true
+
+
+// Instantiate a MAX30100 sensor class
+MAX30100 sensor;
+
+void setup()
+{   
+    M5.begin();
+    M5.Lcd.setRotation(3);
+    //M5.Lcd.setTextColor(WHITE);
+    M5.Lcd.setTextSize(3);
+    M5.Lcd.print("MAX30100");
+  //M5.Lcd.clear(BLACK);
+    M5.Lcd.setCursor(0, 40);
+    Serial.begin(115200);
+
+    Serial.print("Initializing MAX30100..");
+
+    // Initialize the sensor
+    // Failures are generally due to an improper I2C wiring, missing power supply
+    // or wrong target chip
+    if (!sensor.begin()) {
+        Serial.println("FAILED");
+        for(;;);
+    } else {
+        Serial.println("SUCCESS");
+    }
+
+    // Set up the wanted parameters
+    sensor.setMode(MAX30100_MODE_SPO2_HR);
+    sensor.setLedsCurrent(IR_LED_CURRENT, RED_LED_CURRENT);
+    sensor.setLedsPulseWidth(PULSE_WIDTH);
+    sensor.setSamplingRate(SAMPLING_RATE);
+    sensor.setHighresModeEnabled(HIGHRES_MODE);
+}
+
+void loop()
+{
+    uint16_t ir, red;
+
+    sensor.update();
+
+    while (sensor.getRawValues(&ir, &red)) {
+        Serial.print(ir);
+        Serial.print('\t');
+        Serial.println(red);
+
+    }
+}
